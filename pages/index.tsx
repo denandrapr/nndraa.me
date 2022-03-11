decf4ebc263a84c7539016e59f07e4b9ebede305
Denandra Prasetya <denandra1628@gmail.com>
2022-03-11 10:29:48 +0700

    Update index.tsx
    add github

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello, My name is <a href="https://pornhub.com">Denandra</a>
+          Hello, My name is <a href="https://github.com/denandrapr">Denandra</a>
         </h1>
       </main>
 
